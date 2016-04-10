a155d799f62dd482873dcf66db3f0b1e4322cb9d
li_z <274249651@qq.com>
2016-04-10 15:20:34 +0800

    suppress warnings

--- src/main/java/sample/Test.java
+++ src/main/java/sample/Test.java
@@ -4,6 +4,7 @@ import sample.VirtualSample.Sample;
 
 public class Test {
 
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Sample sample = new JavaSample(1);
         System.out.println(sample.add(2, 3));
